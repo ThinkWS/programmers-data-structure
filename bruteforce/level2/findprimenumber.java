ae7093fbf2a85ce53bc8c4ce5448f2f71a9ad64c
ThinkWS <165904905+ThinkWS@users.noreply.github.com>
2026-02-20 20:31:06 +0900

    Create findprimenumber.java

---
+++ bruteforce/level2/findprimenumber.java
@@ -0,0 +1,50 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    
+    Set<Integer> set = new HashSet<>();
+    boolean[] visited;
+    
+    public int solution(String numbers) {
+        
+        visited = new boolean[numbers.length()];
+        
+        dfs(numbers, "");
+        
+        int count = 0;
+        
+        for (int num : set) {
+            if (isPrime(num)) count++;
+        }
+        
+        return count;
+    }
+ 
+    private void dfs(String numbers, String current) {
+        
+        if (!current.equals("")) {
+            set.add(Integer.parseInt(current));
+        }
+        
+        for (int i = 0; i < numbers.length(); i++) {
+            
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(numbers, current + numbers.charAt(i));
+                visited[i] = false;
+            }
+        }
+    }
+    
+    private boolean isPrime(int num) {
+        
+        if (num < 2) return false;
+        
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        
+        return true;
+    }
+}
